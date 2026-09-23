26ff74bc2f00c928aea621aff77f49765bbdf914
Michael D'Amico <michaeldamico666@gmail.com>
2026-09-23 02:22:05 +0200

    Setup iniziale progetto Spring Boot

---
+++ src/main/java/com/michaeldamico/gestionale/GestionaleTennistavoloApplication.java
@@ -0,0 +1,13 @@
+package com.michaeldamico.gestionale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleTennistavoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleTennistavoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaeldamico/gestionale/GestionaleTennistavoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaeldamico.gestionale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleTennistavoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
